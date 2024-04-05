3e439647e753701dfa349938b2d88beaf7678a1c
Dmytro Korolik <dimytry.korolik@gmail.com>
2024-04-05 13:45:40 +0300

    base-file-with-some-logiq

---
+++ selectCodes.js
@@ -0,0 +1,50 @@
+const gpuDefaultData = {
+  GTA_V_heightFullHD,
+  GTA_V_ultraFullHD,
+  GTA_V_ultraHD4K,
+  Cyberpunk_2077_heightFullHD,
+  Cyberpunk_2077_ultraFullHD,
+  Cyberpunk_2077_ultraHD4K,
+  God_of_War_heightFullHD,
+  God_of_War_ultraFullHD,
+  God_of_War_ultraHD4K,
+  gpu_type,
+  gpu_series,
+  gpu_memory_capacity,
+  gpu_memory_type,
+  vr_support,
+  test_3DMark06,
+  test_3DMark_Vantage_P,
+};
+
+const gpu_type = {
+  3749: 3749, //"интегрированная",
+  3750: 3750, //"дискретная",
+  7889: 7889, //"SLI / CrossFire",
+  6277: 6277, //"AMD Dual Graphics",
+};
+
+const gpu_series = {
+  6964: 6964, //"Intel GMA",
+  3805: 3805, //"Intel HD Graphics",
+  7647: 7647, //"Intel Iris Graphics",
+  29269: 29269, //"Intel Arc",
+  3815: 3815, //"AMD Radeon",
+  8077: 8077, //"AMD FirePro",
+  3819: 3819, //"NVIDIA GeForce",
+  3947: 3947, //"NVIDIA Quadro",
+  26808: 26808, //"NVIDIA RTX",
+  21326: 21326, //"Qualcomm Adreno",
+  25051: 25051, //"Apple",
+  29740: 29740, //"Mali",
+};
+
+const memory_type = {
+  3846: 3846, //"нет данных",
+  3752: 3752, //"GDDR3",
+  3754: 3754, //"GDDR5",
+  16022: 16022, //"GDDR5X",
+  20913: 20913, //"GDDR6",
+  30490: 30490, //"GDDR6X",
+  19502: 19502, //"HBM2",
+};
